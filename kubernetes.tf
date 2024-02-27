6d72eb3e480637a79cb65f81dce9c07f4eab3863
tuncaytas <tnc.tas@gmail.com>
2024-02-27 17:22:09 -0600

    helm

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,11 +3,14 @@
 
 
 
-data "terraform_remote_state" "eks" {
-  backend = "local"
-  config = {
-    path = "../learn-terraform-provision-eks-cluster/terraform.tfstate"
-  }
+
+# Retrieve EKS cluster configuration
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 provider "kubernetes" {
